8c7233ca0f69d55d1037b2ebc44d11355841df7c
Ben Governali <bgovern93@gmail.com>
2019-06-12 15:56:21 -0400

    added more scopes to sign in button to request additional info from slack. slack response now stores in a data variable

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,13 @@ const express = require('express'),
 
 require('dotenv').config();
 
-/* GET users listing. */
+/* GET oauth token and user info. */
 router.get('/auth/slack', function(req, res) {
   request.get(`https://slack.com/api/oauth.access?client_id=${process.env['CLIENT_ID']}&client_secret=${process.env['CLIENT_SECRET']}&code=${req.query.code}`, function (error, response, body) {
-    console.log(response.statusCode);
-    console.log('this is the body!!!', body);
+    const data = JSON.parse(body);
+    console.log(data);
+    console.log(data.access_token);
+    return data;
   })
 });
 
