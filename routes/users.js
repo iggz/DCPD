1072857d1610f0f2620c72d2b99313a894684bd0
Ben Governali <bgovern93@gmail.com>
2019-06-12 15:18:33 -0400

    slack sign in now requests and receives oauth tokens

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express'),
+  request = require('request');
+  router = express.Router();
+
+require('dotenv').config();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/auth/slack', function(req, res) {
+  request.get(`https://slack.com/api/oauth.access?client_id=${process.env['CLIENT_ID']}&client_secret=${process.env['CLIENT_SECRET']}&code=${req.query.code}`, function (error, response, body) {
+    console.log(response.statusCode);
+    console.log('this is the body!!!', body);
+  })
 });
 
 module.exports = router;
