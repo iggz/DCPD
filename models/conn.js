ab4c96046f4c9df89100ffb45c161b3b6c1529b5
Ben Governali <bgovern93@gmail.com>
2019-06-17 14:42:24 -0400

    changed database user password

--- models/conn.js
+++ models/conn.js
@@ -7,7 +7,8 @@ const pgp = require('pg-promise') ({
 const options = {
     host: 'localhost',
     database: 'dcpd',
-    user: 'dcpdadmin'
+    user: 'dcpdadmin',
+    password: 'basedgod'
 }
 
 const db = pgp(options);
