2dad49e04792427892ac724df28720e77c80c3e6
iggz <46656715+iggz@users.noreply.github.com>
2019-06-18 12:50:53 -0400

    update conn.js
    username

--- models/conn.js
+++ models/conn.js
@@ -7,7 +7,8 @@ const pgp = require('pg-promise') ({
 const options = {
     host: 'localhost',
     database: 'dcpd',
-    user: 'ubuntu'
+    user: 'dcpdadmin',
+    password: 'secret'
 }
 
 const db = pgp(options);
