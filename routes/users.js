94adb1ea2ef001873d19224efcaa2d03b77a914b
igor popenov <ipopenov@protonmail.com>
2019-08-22 17:50:00 -0400

    Added images for the README

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express'),
   request = require('request');
-  router = express.Router();
-  userController = require('../controllers/userController');
-  User = require('../models/usersModel');
+router = express.Router();
+userController = require('../controllers/userController');
+User = require('../models/usersModel');
 
 require('dotenv').config();
 
@@ -13,21 +13,21 @@ router.get('/auth/slack', async (req, res) => {
     const team_id = data.team.id;
     //if user is member of slack team, set session variables
     //then check to see if they are already in database
-    if(team_id == process.env['TEAM_ID']) {
+    if (team_id == process.env['TEAM_ID']) {
       req.session.email = data.user.email;
       req.session.bearer = data.access_token;
       req.session.status = data.ok;
       req.session.is_logged_in = true;
       const user = new User(null, null, null, req.session.email);
       userCheck = await user.checkProfile();
-      if(userCheck.first_name == null) {
+      if (userCheck.first_name == null) {
         res.redirect('/users/signup');
       }
       else {
         res.redirect('/');
       }
     } else {
-    res.redirect('/');
+      res.redirect('/');
     }
   })
 });
